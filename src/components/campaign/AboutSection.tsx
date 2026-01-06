974714ef6a48ece599a2fc59d17fff03072ed4b2
mcardonamapei17 <m.cardonamapei@gmail.com>
2026-01-06 15:28:11 -0500

    cambios pequeños

--- src/components/campaign/AboutSection.tsx
+++ src/components/campaign/AboutSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import communityPhoto from "@/assets/persona.jpg";
+import communityPhoto from "@/assets/ali.jpg";
 
 const AboutSection = () => {
   return (
